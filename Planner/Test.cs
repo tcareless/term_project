bbbe646a4bd96a7b5658e9332b73dacbd530120f
ayush777777725 <ayush777777725@gmail.com>
2023-12-01 15:10:46 -0500

    modifed main() function to be more informational

--- Planner/Test.cs
+++ Planner/Test.cs
@@ -9,30 +9,45 @@ namespace Planner
 {
     internal class Test
     {
-        static void Main()
+        class Program
         {
-            // Set up the journey details
-            string startCity = "Windsor";
-            string endCity = "London";
-            TransportationPlanner.ShipmentType shipmentType = TransportationPlanner.ShipmentType.LTL; // Example: LTL shipment
-            double transportRatePerPalletPerKm = 0.5; // Example rate per pallet per km
-            int numberOfPallets = 10;
-            double reeferCharge = 0.05; 
-            bool isReeferRequired = false; // Example: Reefer van is required
+            static void Main()
+            {
+                // Define the input parameters and constraints for the shipment
+                string startCity = "Windsor";
+                string endCity = "Kingston";
+                TransportationPlanner.ShipmentType shipmentType = TransportationPlanner.ShipmentType.LTL; // Example: LTL shipment
+                double transportRatePerPalletPerKm = 0.5; // Example rate per pallet per km
+                int numberOfPallets = 5; // Example: 5 pallets for LTL
+                double reeferCharge = 0.05; // Example: 5% reefer charge
+                bool isReeferRequired = true; // Example: Reefer van is required
 
-            // Calculate the number of days for the journey
-            double totalDays = TransportationPlanner.CalculateDays(startCity, endCity, shipmentType);
-            Console.WriteLine($"Total days from {startCity} to {endCity}: {totalDays}");
+                // Print the input parameters and constraints
+                Console.WriteLine("Journey Details:");
+                Console.WriteLine($"Start City: {startCity}");
+                Console.WriteLine($"End City: {endCity}");
+                Console.WriteLine($"Shipment Type: {shipmentType}");
+                Console.WriteLine($"Transport Rate Per Pallet Per Km: ${transportRatePerPalletPerKm}");
+                Console.WriteLine($"Number of Pallets: {numberOfPallets}");
+                Console.WriteLine($"Reefer Charge: {(reeferCharge * 100)}%");
+                Console.WriteLine($"Is Reefer Required: {isReeferRequired}");
+                Console.WriteLine();
 
-            // Calculate the total distance of the journey
-            double totalDistance = TransportationPlanner.CalculateDistance(startCity, endCity);
-            Console.WriteLine($"Total distance from {startCity} to {endCity}: {totalDistance} km");
+                // Calculate and print the number of days for the journey
+                double totalDays = TransportationPlanner.CalculateDays(startCity, endCity, shipmentType);
+                Console.WriteLine($"Total days from {startCity} to {endCity}: {totalDays}");
 
-            // Calculate the total cost of the shipment
-            double totalCost = TransportationPlanner.CalculateCost(startCity, endCity, shipmentType, transportRatePerPalletPerKm, numberOfPallets, reeferCharge, isReeferRequired);
-            Console.WriteLine($"Total cost for the shipment from {startCity} to {endCity}: ${totalCost}");
+                // Calculate and print the total distance of the journey
+                double totalDistance = TransportationPlanner.CalculateDistance(startCity, endCity);
+                Console.WriteLine($"Total distance from {startCity} to {endCity}: {totalDistance} km");
 
-            Console.ReadLine(); 
+                // Calculate and print the total cost of the shipment
+                double totalCost = TransportationPlanner.CalculateCost(startCity, endCity, shipmentType, transportRatePerPalletPerKm, numberOfPallets, reeferCharge, isReeferRequired);
+                Console.WriteLine($"Total cost for the shipment from {startCity} to {endCity}: ${totalCost}");
+
+                Console.ReadLine(); // Keep the console window open
+            }
         }
+
     }
 }
